6d7759e1bef0cf0b944055bdd81915845fc8e3e8
James Humphries <James@james-humphries.co.uk>
2023-01-19 10:04:30 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+resource "aws_instance" "" {
+  ami           = "ami-08633248b535ef169"
+  instance_type = "t2.micro"
+}
