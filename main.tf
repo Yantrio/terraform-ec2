731273575d00341e6845614eb30438211a716c6c
James Humphries <James@james-humphries.co.uk>
2023-01-19 10:11:21 +0000

    forgot region

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "eu-west-1"
+}
+
 resource "aws_instance" "my_instance" {
   ami           = "ami-08633248b535ef169"
   instance_type = "t2.micro"
